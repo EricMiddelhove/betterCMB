85acaeb428631788fadaea14cd5ab5e758316efe
Eric Middelhove <eric.middelhove@icloud.com>
2020-02-16 16:55:05 +0100

    Scrollen automatisiert

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 console.log("file loaded");
 
-let nxtBtn = document.getElementById('nxt');
-let preBtn = document.getElementById('prev');
-
 let slideContainer = document.querySelector('.slider-container');
 let slides = document.querySelectorAll('.slides img');
 let slide = document.querySelector('.slides')
@@ -12,11 +9,7 @@ console.log(slideWidth);
 
 let counter = 0;
 
-console.log(nxtBtn);
-console.log(preBtn);
-
-nxtBtn.addEventListener('click',()=>{
-
+setInterval(function(){
     slide.style.transition = "transform 0.4s ease-in-out";
     if(counter < slides.length - 1){
         counter++;
@@ -25,18 +18,5 @@ nxtBtn.addEventListener('click',()=>{
         counter = 0;
         slide.style.transform = "translate(" + (-slideWidth * counter) + "px)";
     }
-    
-});
 
-preBtn.addEventListener('click', ()=>{
-
-    slide.style.transition = "transform 0.4s ease-in-out";
-    if(counter > 0){
-        counter--;
-        slide.style.transform = "translate(" + (-slideWidth * counter) + "px)";
-    }else{
-        counter = slides.length-1;
-        slide.style.transform = "translate(" + (-slideWidth * counter) + "px)";
-    }
-    
-});
+},3000);
