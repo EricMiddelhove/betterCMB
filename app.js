4eaffa0aba3f7e3fe52c93418f528d498d45c07f
maxmi712 <maxmi712@web.de>
2020-02-19 08:23:13 +0100

    anmeldung add

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("file loaded");
+
+let slideContainer = document.querySelector('.slider-container');
+let slides = document.querySelectorAll('.slides img');
+let slide = document.querySelector('.slides')
+
+let slideWidth = slides[0].clientWidth;
+console.log(slideWidth);
+
+let counter = 0;
+
+setInterval(function(){
+    slide.style.transition = "transform 1s ease-in-out";
+    if(counter < slides.length - 1){
+        counter++;
+        slide.style.transform = "translate(" + (-slideWidth * counter) + "px)";
+    }else{
+        counter = 0;
+        slide.style.transform = "translate(" + (-slideWidth * counter) + "px)";
+    }
+
+},5000);
